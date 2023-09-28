96a2a81a28e1267633f94179a53aa71547c353a6
lcozdenm <loic.cozdenmat@gmail.com>
2023-09-28 21:49:09 +0200

    fixed data_race

--- philo/src/philo_forks.c
+++ philo/src/philo_forks.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 16:10:39 by lcozdenm          #+#    #+#             */
-/*   Updated: 2023/09/28 19:39:41 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/09/28 21:46:35 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,30 @@ t_fork	*create_fork(void)
 
 int	take_forks(t_fork *l_fork, t_fork *r_fork)
 {
-	int	fail;
-
-	fail = 0;
 	pthread_mutex_lock(&l_fork->lock);
-	if 
-	pthread_mutex_lock(&r_fork->lock);
-	if (!l_fork->is_used && !r_fork->is_used)
-	{
+	if (!l_fork->is_used)
 		l_fork->is_used = TRUE;
+	else
+	{
+		pthread_mutex_unlock(&l_fork->lock);
+		return (1);
+	}
+	pthread_mutex_unlock(&l_fork->lock);
+	pthread_mutex_lock(&r_fork->lock);
+	if (!r_fork->is_used)
+	{
 		r_fork->is_used = TRUE;
+		pthread_mutex_unlock(&r_fork->lock);
 	}
 	else
-		fail = 1;
-	pthread_mutex_unlock(&l_fork->lock);
-	pthread_mutex_unlock(&r_fork->lock);
-	return (fail);
+	{
+		pthread_mutex_unlock(&r_fork->lock);
+		pthread_mutex_lock(&l_fork->lock);
+		l_fork->is_used = FALSE;
+		pthread_mutex_unlock(&l_fork->lock);
+		return (1);
+	}
+	return (0);
 }
 
 int	use_forks(t_philo *philo)
